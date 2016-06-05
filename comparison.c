8a38049889c47192500110827ce6ca04229df6c3
Richard Pietrzak <rpietrzak256@gmail.com>
2016-06-04 23:57:38 -0400

    STFT now takes in float array instead of SNDFILE,
    also now has correct blocksize output of blocksize/2 + 1 instead of blocksize/2

--- comparison.c
+++ comparison.c
@@ -26,23 +26,31 @@ int ExtractMelody(char* filename)
 	SNDFILE * f = sf_open(filename, SFM_READ, &info);
 	if( !f ){
 		printf("ERROR: could not open %s for processing\n", filename );
-		return 0;
+		return -1;
 	}
 	if(info.channels != 1){
 		printf("ERROR: .wav file must be Mono. Code can't currently handle multi-channel audio.\n");
 		sf_close( f );
-		return 0;
-	}
-	PrintAudioMetadata(&info);
-
-	//run STFT on input
-	double** AudioData = NULL;
-	int size = STFT(f, info, sampleSize, &AudioData);
-	if( !size ) {
-		printf("ERROR: ReadAudioFile failed for %s\n", filename);
+		return -1;
 	}
 	
-	printf("size is %d\n", size);
+	PrintAudioMetadata(&info);
+
+	double* input = fftw_malloc( sizeof(double) * info.frames);
+	sf_count_t numRead = sf_readf_double( f, input, info.frames );
+	if ( numRead != info.frames ) {
+		printf("ERROR: input received incorrect amount of data\n");
+		return -1;
+	}
+
+	sf_close( f );
+
+	double** AudioData = NULL;
+	int size = STFT(&input, info, sampleSize, &AudioData);
+	fftw_free( input );
+
+	//printf("data: %f %f %f %f\n", AudioData[0][0], AudioData[0][1], AudioData[1][0], AudioData[1][1]);
+
 
 	//free data
 	int i;
@@ -54,70 +62,51 @@ int ExtractMelody(char* filename)
 	return size;
 }
 
-int STFT(SNDFILE * f, SF_INFO info, int blocksize, double*** dft_data)
-{
-	//reads in .wav, returns FFT by reference through dft_data, returns size of dft_data
-	sf_count_t i;
-	sf_count_t j;
+//reads in .wav, returns FFT by reference through dft_data, returns size of dft_data
+int STFT(double** input, SF_INFO info, int blocksize, double*** dft_data) {
+    int i;
+    int j;
 
-	double* fftw_in = fftw_malloc( sizeof(double) * blocksize * info.channels );
-	if ( !fftw_in ) {
-		printf("ERROR: fftw_malloc 1 failed\n");
-		sf_close( f );
-		return 0;
-	}
+    double* fftw_in = fftw_malloc( sizeof( double ) * blocksize * info.channels );
+    fftw_complex* fftw_out = fftw_malloc( sizeof( fftw_complex ) * blocksize );
 
-	fftw_complex* fftw_out = fftw_malloc( sizeof(fftw_complex) * blocksize );
-	if ( !fftw_out ) {
-		printf("ERROR: fftw_malloc 2 failed\n");
-		fftw_free( fftw_in );
-		sf_close( f );
-		return 0;
-	}
-
-	fftw_plan plan = fftw_plan_dft_r2c_1d( blocksize, fftw_in, fftw_out, FFTW_MEASURE );
-	if ( !plan ) {
-		printf("ERROR: Could not create plan\n");
-		fftw_free( fftw_in );
-		fftw_free( fftw_out );
-		sf_close( f );
-		return 0;
-	}
-
-	//allocate space for array to return
+    fftw_plan plan  = fftw_plan_dft_r2c_1d( blocksize, fftw_in, fftw_out, FFTW_MEASURE );
+ 
+    //malloc space for dft_data
 	int numBlocks = (int)(ceil(info.frames / (double)blocksize));
-    (*dft_data) = malloc( sizeof(double*) * numBlocks * blocksize/2 );
-	for(i = 0; i < (numBlocks * blocksize / 2.0); i++){
+    (*dft_data) = malloc( sizeof(double*) * numBlocks * (blocksize/2 + 1) );
+	for(i = 0; i < (numBlocks * (blocksize/2 + 1)); i++){
 		(*dft_data)[i] = malloc(sizeof(double) * 2);
 	}
-	//printf("transform splits file into %d slices\n", numBlocks);
+ 
+	int blockoffset;
+    // Process each chunk of the input
+    for(i = 0; i < numBlocks; i++){
 
-	sf_count_t numRead;
-	for(i = 0; i < numBlocks; i++){
+		// Copy the chunk into our buffer
+		blockoffset = i*blocksize;
+		for(j = 0; j < blocksize; j++) {
 
-		numRead = sf_readf_double( f, fftw_in, blocksize );
-
-		if ( numRead < blocksize ) {
-			//reached last block, which has less than blocksize frames
-			//Pad with 0 so fft is same size as other blocks
-			for( j = numRead; j < blocksize; j++ ) {
-				fftw_in[j] = 0.0;
+			if(blockoffset + j < info.frames) {
+				fftw_in[j] = (*input)[blockoffset + j];
+			} else {
+				//reached end of input
+				//Pad with 0 so fft is same size as other blocks
+				fftw_in[j] = 0.0; 
 			}
 		}
 
 		fftw_execute( plan );
 
-		for(j = 0; j < blocksize/2; j++){
-			//printf("%ld\n", i*numBlocks + j);
-			(*dft_data)[i*(blocksize/2) + j][0] = fftw_out[j][0];
-			(*dft_data)[i*(blocksize/2) + j][1] = fftw_out[j][1];
-		}
+		for (j = 0; j < blocksize/2 + 1; j++) {
+			(*dft_data)[i*(blocksize/2 + 1) + j][0] = fftw_out[j][0];
+			(*dft_data)[i*(blocksize/2 + 1) + j][1] = fftw_out[j][1];
+		}	
 	}
 
 	fftw_destroy_plan( plan );
 	fftw_free( fftw_in );
-	fftw_free( fftw_out);
-	sf_close( f );
+	fftw_free( fftw_out );
 
-    return numBlocks * blocksize/2;
+	return numBlocks * (blocksize/2 + 1);
 }
--- comparison.h
+++ comparison.h
@@ -3,4 +3,4 @@
 
 void PrintAudioMetadata(SF_INFO * file);
 int ExtractMelody(char* filename);
-int STFT(SNDFILE * f, SF_INFO info, int blocksize, double*** dft_data);
+int STFT(double** signal, SF_INFO info, int blocksize, double*** dft_data);
