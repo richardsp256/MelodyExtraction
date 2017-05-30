8cb5934856bc6b5b94df91f907598db02e3ce130
Matthew Abruzzo <matthewabruzzo@gmail.com>
2017-05-30 12:18:12 -0400

    Consolidation of lists.c and candidates.c part 2
    Not sure why this didn't get committed with everything else

--- lists.h
+++ lists.h
@@ -1,5 +1,11 @@
-#ifndef CANDIDATES_H
-#define CANDIDATES_H
+#ifndef LISTS_H
+#define LISTS_H
+
+struct orderedList{
+	double *array;
+	int max_length;
+	int length;
+};
 
 struct candidate{
   double frequency;
@@ -13,8 +19,16 @@ struct candidateList{
   int max_length;
   int length;
 };
-#endif /*CANDIDATES_H*/
 
+#endif /*LISTS_H*/
+
+struct orderedList orderedListCreate(int max_length);
+void orderedListDestroy(struct orderedList list);
+double orderedListGet(struct orderedList list, int index);
+int bisectInsertionSearch(struct orderedList list, double value);
+void orderedListInsert(struct orderedList *list, double value);
+void orderedListDeleteEntries(struct orderedList *list, int start, int stop);
+void orderedListPrint(struct orderedList list);
 
 struct candidateList* candidateListCreate(int max_length);
 void candidateListDestroy(struct candidateList* list);
