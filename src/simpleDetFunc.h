9d9a0095463f1efb89f0e8db417f72e859023f24
Matthew Abruzzo <matthewabruzzo@gmail.com>
2019-08-24 14:37:20 -0400

    fixed a formatting mistake in the docstrings.

--- src/simpleDetFunc.h
+++ src/simpleDetFunc.h
@@ -3,38 +3,38 @@
 /// The implementation is not memory efficient and sigma optimization can
 /// definitely be improved to be much faster
 ///
-/// \param[in] correntropyWinSize The window size for the calculation of
+/// @param[in] correntropyWinSize The window size for the calculation of
 ///            correntropy (specified as a number of audio frames). In this
 ///            implementation, the maximum lag value is also given by this
 ///            value. The paper suggests a value of samplerate/80 if
 ///            minFreq = 80 Hz
-/// \param[in] interval The hopsize, h, used in calculation of detection
+/// @param[in] interval The hopsize, h, used in calculation of detection
 ///            function (specified as a number of audio frames). This is
 ///            assumed to be the same as the interval h for optimizing sigma.
 ///            The paper suggests a value of 5ms (~55 samples when sampleRate
 ///            is 11025 Hz)
-/// \param[in] scaleFactor The constant used to estimate the optimized sigma.
+/// @param[in] scaleFactor The constant used to estimate the optimized sigma.
 ///            For Silverman's rule of thumb, this is (4./3.)^(1/5)
-/// \param[in] sigWindowSize The size of the window used to compute the
+/// @param[in] sigWindowSize The size of the window used to compute the
 ///            optimized sigma for the kernel (specified as a number of audio
 ///            frames). The paper suggests a window size of 7s (7*sampleRate).
-/// \param[in] numChannels The number of channels in the FilterBank. This must
+/// @param[in] numChannels The number of channels in the FilterBank. This must
 ///            be positive. The paper suggests a value of 64. If set to 1, then
 ///            minFreq and maxFreq must be equal; the filterbank will have 1
 ///            channel with central frequency equal to minFreq and maxFreq.
-/// \param[in] minFreq The lowest frequency (in Hz) included in the bandwidth
+/// @param[in] minFreq The lowest frequency (in Hz) included in the bandwidth
 ///            of a channel of the filterbank. The paper suggests 80 Hz.
-/// \param[in] maxFreq The maximum frequency (in Hz) included in the bandwidth
+/// @param[in] maxFreq The maximum frequency (in Hz) included in the bandwidth
 ///            of a channel of the filterbank. The paper suggests 4000 Hz.
-/// \param[in] sampleRate The samplerate of the audio data passed to data.
-/// \param[in] dataLength The length of the input audio data.
-/// \param[in] data The input audio data for which the detection function
+/// @param[in] sampleRate The samplerate of the audio data passed to data.
+/// @param[in] dataLength The length of the input audio data.
+/// @param[in] data The input audio data for which the detection function
 ///            should be computed.
-/// \param[in] detFunctionLength The expected length of the data function
-/// \param[out] detFunction An array that will be modified to have values of
+/// @param[in] detFunctionLength The expected length of the data function
+/// @param[out] detFunction An array that will be modified to have values of
 ///             the detection function
 ///
-/// \return Returns 1 for success and -1 for failure.
+/// @return Returns 1 for success and -1 for failure.
 ///
 /// We made some assumptions about parameters mentioned in the paper:
 ///    1. We assume that the interval h for the sigmaOptimizer is the same as 
--- src/testOnset.h
+++ src/testOnset.h
@@ -5,16 +5,16 @@
 /// Internal helper function that identifies transients from the provided
 /// detection function
 ///
-/// \param[in] detection_func Array of detection function values.
-/// \param[in] len Number of values in detection_func
-/// \param[out] transients A pointer to an initially empty intList that will be
+/// @param[in] detection_func Array of detection function values.
+/// @param[in] len Number of values in detection_func
+/// @param[out] transients A pointer to an initially empty intList that will be
 ///             filled with integers that indicate the interval in the 
 ///             detection function in which a transient has been identified.
 ///             The transients alternate between being onsets and offsets. The
 ///             first transient is always an onset while the last transient is
 ///             always on offset (there are always an even number of values).
 ///
-/// \return Returns the length of transients. A value of 0 or smaller indicates
+/// @return Returns the length of transients. A value of 0 or smaller indicates
 ///         that an error occured
 ///
 /// Note: We added an extra step not detailed in the paper in which we
@@ -28,16 +28,17 @@ int detectTransients(float* detection_func, int len, intList* transients);
 /// Identifies pairs of onsets and offsets from audio data using the algorithm
 /// published by Chang & Lee (2016)
 ///
-/// \param[in] audioData The audio data for which the transients should be
+/// @param[in] audioData The audio data for which the transients should be
 ///            identified
-/// \param[in] size Length of audioData
-/// \param[in] samplerate The sample rate of the audio data (in Hz)
-/// \param[out] transients A pointer to an initially empty intList. This list
+/// @param[in] size Length of audioData
+/// @param[in] samplerate The sample rate of the audio data (in Hz)
+/// @param[out] transients A pointer to an initially empty intList. This list
 ///             will be filled with integers corresponding to the audio frame
 ///             where transients occured. The transients alternate between
 ///             onsets and offsets. The first value is always an onset while
 ///             the last value is always an offset.
 ///
-/// Note: This uses the default settings mentioned in the method paper
+/// @par Note:
+/// This uses the default settings mentioned in the method paper
 int pairwiseTransientDetection(float *audioData, int size, int samplerate,
 			       intList* transients);
